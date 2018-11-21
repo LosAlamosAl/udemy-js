60dc465c41036811e28a56c88408062fcc175884
Los Alamos Al <al@losalamosal.me>
2018-11-20 17:01:54 -0700

    Final working version

--- number-guesser/app.js
+++ number-guesser/app.js
@@ -1,3 +1,5 @@
+// Disable some ESLint checks
+/* eslint-disable no-multi-spaces */
 /* eslint-disable no-console */
 /* eslint-disable linebreak-style */
 /* eslint-disable no-param-reassign */
@@ -7,85 +9,111 @@
 /* eslint-disable no-undef */
 // @ts-nocheck
 
-// TODO
-// - remove up/down component from number guess field
-// - match border color of number guess field to message (red and green)
-// - set number guess field to empty at end of game
-// message should say "n is not correct, yuo have m guesses left" (red)
-// message should say "Sorry, game over, the correct answer was n" (red)
-// message should say "n is correct!" (green)
-// when game is over:
-//  - last guess stays in number guess field
-//  - button says PLAY AGAIN
-//  - number guess field disabled
 
-const min = 1;
-const max = 10;
+// Global game state
+const min = 1;    // always 1 or nned to change newRand function
+const max = 12;
 const maxGuesses = 4;
 let goal;
 let guessesLeft;
-let isRunning;
+let isGameOn;
 
-function newGame() {
-  goal = newRand(max);
-  console.log(`Goal: ${goal}`);
-  guessesLeft = maxGuesses;
-  isRunning = true;
-}
-
-function newRand(maxRand) {
-  return Math.floor(Math.random() * Math.floor(maxRand)) + 1;
-}
-
+// UI components
 const guess = document.getElementById('guess-input');
 const submit = document.getElementById('guess-value');
 const message = document.querySelector('.message');
 
-submit.addEventListener('click', checkGuess);
 
-newGame();
+// Start a new game
+function newGame() {
+  goal = newRand(max);
+  console.log(`Goal: ${goal}`);
+  guessesLeft = maxGuesses;
+  isGameOn = true;
+  message.textContent = '';
+  message.style.color = '';    // superfluous
+  guess.value = '';
+  submit.value = 'SUBMIT';
+  guess.style.borderColor = '';
+  guess.disabled = false;
+}
 
+
+// Generate a new random number in the range [1, maxRand]
+function newRand(maxRand) {
+  return Math.floor(Math.random() * Math.floor(maxRand)) + 1;
+}
+
+
+// Got here via a click on the button.
+// It's a dual function button:
+// - if it says SUBMIT, check the guess (isGameOn = true)
+// - if it says PLAY AGAIN, start a new game (isGameOn = false)
 function checkGuess() {
-  if (guess.value === '') return;
+  if (!isGameOn) {   // submit (play again) starts new game
+    newGame();
+    return;
+  }
+  if (guess.value === '') {    // need to set to '' for bogus exponent (e.g. e3)
+    guess.value = '';
+    return;
+  }
   const thisGuess = Number.parseInt(guess.value, 10);
-  console.log(`This guess: ${thisGuess}`);
+  if (thisGuess < min || thisGuess > max) {      // bad number
+    guess.value = '';
+    return;
+  }
 
-  if (thisGuess < min || thisGuess > max) {
-    chooseWisely();
-    return;
-  }
+  // correct guess
   if (thisGuess === goal) {
-    youWin();
+    youWin(thisGuess);
     return;
   }
+
+  // wrong guesss
   guessesLeft -= 1;
   if (guessesLeft === 0) {
-    noGuessesLeft();
+    youLose(thisGuess);
   } else {
-    wrongShowGuessesLeft();
+    guessAgain(thisGuess);
   }
-  guess.value = '';
 }
 
-function chooseWisely() {
-  message.textContent = `Choose wisely, you have ${guessesLeft} guesses left`;
-  message.style.color = '#f00';
-  guess.value = '';
-}
 
-function youWin() {
-  message.textContent = 'You win, play another?';
+function youWin(g) {
+  message.textContent = `${g} is correct!`;
   message.style.color = '#0f0';
-  newGame();
+  guess.style.borderColor = '#0f0';
+  submit.value = 'PLAY AGAIN';
+  guess.disabled = true;
+  isGameOn = false;
 }
 
-function noGuessesLeft() {
-  message.textContent = 'No guesses left, play another?';
+
+function youLose(g) {
+  message.textContent = `Sorry, game over, the correct answer was ${goal}`;
   message.style.color = '#f00';
-  newGame();
+  guess.style.borderColor = '#f00';
+  submit.value = 'PLAY AGAIN';
+  guess.disabled = true;
+  isGameOn = false;
 }
 
-function wrongShowGuessesLeft() {
-  message.textContent = `Incorrect, you have ${guessesLeft} guesses left`;
+
+function guessAgain(g) {
+  message.textContent = `${g} is not correct, you have ${guessesLeft} guesses left`;
+  guess.value = '';
   message.style.color = '#f00';
 }
+
+
+//  Listen for clicks on the button
+submit.addEventListener('click', checkGuess);
+
+//  Show user range of acceptable guesses
+document.querySelector('.max-num').innerHTML = max.toString();
+document.querySelector('.min-num').innerHTML = min.toString();
+
+// Start the first game.
+// From here on out, everything is controlled by the event loop.
+newGame();
