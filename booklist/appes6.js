97683a53e88fa6505b53200a41fc3d1ca7e8afe3
Los Alamos Al <al@losalamosal.me>
2018-11-24 19:14:06 -0700

    Working, needs refinement

---
+++ booklist/appes6.js
@@ -0,0 +1,70 @@
+// Disable some ESLint checks
+/* eslint-disable no-multi-spaces */
+/* eslint-disable no-console */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-alert */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
+// @ts-nocheck
+
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+// Global game state
+const books = [];
+
+// UI components
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const isbn = document.getElementById('isbn');
+const bookForm = document.getElementById('book-form');
+const bookList = document.getElementById('book-list');
+
+//  Listen for submit on form
+bookForm.addEventListener('submit', newBook);
+
+function newBook(e) {
+  e.preventDefault();
+  const nb = new Book(title.value, author.value, isbn.value);
+  books.push(nb);
+  addToList(nb);
+  console.log(nb);
+  console.log(books);
+  clearForm();
+}
+
+function addToList(book) {
+  const row = bookList.insertRow(-1);
+  let cell;
+  cell = row.insertCell(-1);
+  cell.innerText = book.title;
+  cell = row.insertCell(-1);
+  cell.innerText = book.author;
+  cell = row.insertCell(-1);
+  cell.innerText = book.isbn;
+  cell = row.insertCell(-1);
+  cell.innerText = 'X';
+  cell.addEventListener('click', deleteBook);
+}
+
+function deleteBook(e) {
+  books.forEach((b, i) => {
+    if (b.isbn === e.target.parentElement.cells[2].innerText) {
+      books.splice(i, 1);
+    }
+  });
+  e.target.parentElement.remove();
+}
+
+function clearForm() {
+  title.value = '';
+  author.value = '';
+  isbn.value = '';
+}
